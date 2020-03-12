038f8e3db152fabebf120322883c98a2442ec07e
NickKrsk <ggd43@rambler.ru>
2020-03-12 17:15:56 +0700

    -

--- src/application.js
+++ src/application.js
@@ -2,7 +2,17 @@
 const urls = ['one', 'two'];
 
 const render = (state) => {
+  const input = document.querySelector('.form-control');
   const resultElement = document.querySelector('.result');
+  const submit = document.querySelector('.btn');
+  submit.disabled = state.RSSAdditionProcess.submitDisabled;
+
+  if (state.RSSAdditionProcess.valid) {
+    input.style.border = null;
+  } else {
+    input.style.border = "thick solid red";
+  }
+
   /*const filteredNotebooks = filterItems(state.filter, notebooks);
   if (filteredNotebooks.length === 0) {
     resultElement.innerHTML = '';
@@ -16,8 +26,27 @@ const render = (state) => {
 export default () => {
 
   const state = {
-    valid: true,
+    RSSAdditionProcess: {  
+      valid: true,
+      submitDisabled: false,
+    },
   };
+
+  const input = document.querySelector('.form-control');
+  input.addEventListener('keyup', () => {
+    if (input.value === '') {
+      state.RSSAdditionProcess.valid = true;
+      state.RSSAdditionProcess.submitDisabled = true;
+    } else if (!input.value.match(/^\d+$/)) {
+      state.RSSAdditionProcess.valid = false;
+      state.RSSAdditionProcess.submitDisabled = true;
+    } else {
+      state.RSSAdditionProcess.valid = true;
+      state.RSSAdditionProcess.submitDisabled = false;
+    }
+    
+    render(state);
+  });
 
   const form = document.getElementById('point');
   console.log(form);
@@ -27,6 +56,7 @@ export default () => {
     const RSS = formData.get('RSS'); 
     urls.push(RSS);
     console.log(RSS);
+    input.value = "";
     render(state);
   });
   render(state);
