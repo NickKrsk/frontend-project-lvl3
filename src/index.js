9bad4666aaa990a0770cb98a9d60c31d7b33a088
NickKrsk <ggd43@rambler.ru>
2020-03-30 20:48:28 +0700

    -

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import _ from 'lodash';
 
+const render = (state) => {
+
+};
+
+const  app = () => {
+  const state = {
+    valid: true,
+    RSSlist: [],
+    posts: [],
+  };
+
+  console.log(state);
+  const button = document.querySelector('.btn');
+  button.addEventListener('click', () => {
+    //state.processor = processor.value;
+    console.log('click!');
+    render(state);
+  });
+
+  const input = document.querySelector('[name="input_RSS"]');
+  input.addEventListener('input', () => {
+    console.log(input.value);
+    render(state);
+  });
+
+}
+
+app();
+
+/*
 function component() {
     const element = document.createElement('div');
 
@@ -10,3 +40,4 @@ function component() {
   }
   
   document.body.appendChild(component());
+  */
